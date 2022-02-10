b46e4cc9d45b51f4d6f60bda488053df9c13b936
JayVnl <jorrit.v@outlook.com>
2022-02-10 11:19:21 +0100

    added qrcode package, puzzle piece images

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>Camera page</h1>
+  <div class="h-full w-full">
+    <qrcode-stream @decode="onDecode" @init="onInit" />
+  </div>
 </template>
+
+<script>
+import { QrcodeStream } from 'vue3-qrcode-reader'
+
+export default {
+  components: { QrcodeStream },
+  methods: {
+    onDecode(result) {
+      console.log(result);
+    },
+    async onInit(promise) {
+      try {
+        await promise
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          this.error = "ERROR: you need to grant camera access permission"
+        } else if (error.name === 'NotFoundError') {
+          this.error = "ERROR: no camera on this device"
+        } else if (error.name === 'NotSupportedError') {
+          this.error = "ERROR: secure context required (HTTPS, localhost)"
+        } else if (error.name === 'NotReadableError') {
+          this.error = "ERROR: is the camera already in use?"
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = "ERROR: installed cameras are not suitable"
+        } else if (error.name === 'StreamApiNotSupportedError') {
+          this.error = "ERROR: Stream API is not supported in this browser"
+        } else if (error.name === 'InsecureContextError') {
+          this.error = 'ERROR: Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.';
+        } else {
+          this.error = `ERROR: Camera error (${error.name})`;
+        }
+      }
+    }
+  }
+}
+</script>
--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -1,14 +1,36 @@
 <script setup>
 import InfoButton from '../components/buttons/InfoButton.vue'
+
+const data = [
+  {
+    title: "Simple",
+    amount: 4,
+  },
+  {
+    title: "Medium",
+    amount: 8,
+  },
+  {
+    title: "Hard",
+    amount: 12,
+  },
+  {
+    title: "Ambitious",
+    amount: 20,
+  }
+]
 </script>
 
 <template>
   <div class="flex flex-col px-6 py-10 h-full w-full">
     <h1 class="text-center text-3xl">What should be the difficulty of completing the puzzle?</h1>
     <div class="grow"></div>
-    <InfoButton title="Simple" info="Collect 4 puzzle pieces" path="/camera" />
-    <InfoButton title="Medium" info="Collect 8 puzzle pieces" path="/camera" />
-    <InfoButton title="Hard" info="Collect 12 puzzle pieces" path="/camera" />
-    <InfoButton title="Ambitious" info="Collect 20 puzzle pieces" path="/camera" />
+    <InfoButton
+      v-for="(btn, index) in data"
+      :key="'infobutton: ' + index"
+      :title="btn.title"
+      :info="`Collect ${btn.amount} puzzle pieces`"
+      path="/camera"
+    />
   </div>
 </template>
