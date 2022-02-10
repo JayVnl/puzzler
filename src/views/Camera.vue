9c53a6258ccfbae8ec497aaa0b83f6167347ebb0
JayVnl <jorrit.v@outlook.com>
2022-02-10 12:13:04 +0100

    Puzzle piece now shows popup on scan, quick dirty fix

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="h-full w-full">
     <qrcode-stream @decode="onDecode" @init="onInit" />
+    <div
+      v-if="isQrDecoded"
+      class="bg-white flex flex-col absolute left-1/2 top-1/2 w-9/12 h-auto -translate-y-1/2 -translate-x-1/2 px-6 py-4"
+    >
+      <p class="text-xl font-medium mb-4">You found piece number {{ pieceNumber }}</p>
+      <img :src="pieces[pieceNumber - 1]" class="w-full h-auto mb-5" />
+      <button @click="closePopup" class="bg-gray-200 py-3">Close</button>
+    </div>
   </div>
 </template>
 
 <script>
 import { QrcodeStream } from 'vue3-qrcode-reader'
+import firstPiece from '../assets/piece-1.png';
+import secondPiece from '../assets/piece-2.png';
+import thirdPiece from '../assets/piece-3.png';
+import fourthPiece from '../assets/piece-4.png';
 
 export default {
   components: { QrcodeStream },
+  data() {
+    return {
+      isQrDecoded: false,
+      pieceNumber: null,
+      pieces: [
+        firstPiece,
+        secondPiece,
+        thirdPiece,
+        fourthPiece
+      ]
+    }
+  },
   methods: {
     onDecode(result) {
-      console.log(result);
+      this.isQrDecoded = true;
+      this.pieceNumber = result;
+
+      // this.pieceURL = this.getImagePath(result);
     },
+    // getImagePath(number) {
+    //   const path = require('~/assets/piece-1.png');
+    //   console.log(path);
+    //   // return require(`\src\assets\piece-1.png`);
+    // },
     async onInit(promise) {
       try {
         await promise
@@ -35,6 +67,11 @@ export default {
           this.error = `ERROR: Camera error (${error.name})`;
         }
       }
+    },
+    closePopup() {
+      this.isQrDecoded = false;
+      this.pieceNumber = null;
+      this.pieceURL = "";
     }
   }
 }
